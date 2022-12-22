e8d7e39bc62c77c0afeee8c4d83e058ba83e5abd
shittu olumide <40125098+zenUnicorn@users.noreply.github.com>
2022-12-22 16:49:11 +0100

    init file

---
+++ app.py
@@ -0,0 +1,52 @@
+from web3 import Web3
+import json
+
+#using infura to connect to an ethereum node
+infura_url = "https://mainnet.infura.io/v3/8e741b80953647d3ae8c1bde742180f8"
+
+#connecting to the blockchain
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+#To check if it is connected
+print(web3.isConnected())
+
+#read the number of ethereum blocks
+print(web3.eth.blockNumber)
+
+#reading live account balance of a live wallet, it returns wei
+balance = web3.eth.getBalance("0x05B4CCe36a206C8419B84C27Fd22bfAF64Adc268") #my wallet
+#We need to convert it to ether
+print(web3.fromWei(balance, 'ether'))
+
+
+#Day 2
+#Interact with Smart Contract in web3, we need the sm ABI and Address
+abi = json.loads('[{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_releaseTime","type":"uint256"}],"name":"mintTimelocked","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]')
+address = "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07"
+
+contract = web3.eth.contract(address=address, abi=abi)
+print(contract)
+
+#call() function reads data from the blockchain, send() function writes data to the blockchain
+# let's get the total supply of the smart contract
+totalSupply = contract.functions.totalSupply().call()
+print(web3.fromWei(totalSupply, "ether")) #converting the result to wei (18 decimal places)
+print(contract.functions.name().call())  #getting the contract name
+print(contract.functions.symbol().call())  #getting the contract symbol
+
+#lets interact with the contract and use one of it's function, we will get the balance of the biggest holder of OMG
+HolderBalance = contract.functions.balanceOf('0x23735750a6ed0119e778d9bb969137df8cc8c3d1').call()
+print(web3.fromWei(HolderBalance, 'ether'))
+
+
+#Day 3
+
+
+
+
+
+
+
+
+
+ 
