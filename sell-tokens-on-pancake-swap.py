eb3e078bd4a566cfe3d4f370f1586c50ccc693ca
shittu olumide <40125098+zenUnicorn@users.noreply.github.com>
2023-01-02 01:50:39 +0100

    Update sell-tokens-on-pancake-swap.py

--- sell-tokens-on-pancake-swap.py
+++ sell-tokens-on-pancake-swap.py
@@ -1,5 +1,94 @@
-Tutorial(How to sell token on pancakeswap in python): https://www.youtube.com/watch?v=ehWDvw0jMzo
-Blockchain & A.I.- send Ethereum with just a smile: https://www.youtube.com/watch?v=krZthWO7_3Q&list=PLAgQsXt06PBghC4Sn_cWDrYiAvjn5tCSX&index=12
-CodeWithJoe Playlist: https://www.youtube.com/watch?v=bqrdDrZXNTs&list=PLAgQsXt06PBghC4Sn_cWDrYiAvjn5tCSX
-CodewithJoe Videos: https://www.youtube.com/@CodeWithJoe/videos
-TokenLaunch SniperBo: https://www.youtube.com/watch?v=jY3iVnQPqsg
+#so the project is going to be like this, we run the script, we inout the CA of the toke we want to sell, it prints out the balance of that
+#token in our wallet, then asks how much of that token do we want to sell, we supply that it then sells it, convert our funds back to BNB
+
+from web3 import Web3
+import json
+import time
+import config
+
+#note that pancakeswap is a copy of uniswap solidity contract
+
+#connet to BSC node
+BSC = "https://bsc-dataseed.binance.org/" #use infura for eth
+web3 = Web3(Web3.HTTPProvider(BSC)) #instance of the web3 class
+
+print(web3.isConnected()) #check if we are connected to the blockchain
+
+
+#We need the Router protocol smart contract address, it starts with 0x10
+#pancakeswap router
+pancakeRouterContractAddress = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+#we also need the contract abi
+abi = '[{"inputs":[{"internalType":"address","name":"_factory","type":"address"},{"internalType":"address","name":"_WETH","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"WETH","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"},{"internalType":"uint256","name":"amountADesired","type":"uint256"},{"internalType":"uint256","name":"amountBDesired","type":"uint256"},{"internalType":"uint256","name":"amountAMin","type":"uint256"},{"internalType":"uint256","name":"amountBMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"addLiquidity","outputs":[{"internalType":"uint256","name":"amountA","type":"uint256"},{"internalType":"uint256","name":"amountB","type":"uint256"},{"internalType":"uint256","name":"liquidity","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"amountTokenDesired","type":"uint256"},{"internalType":"uint256","name":"amountTokenMin","type":"uint256"},{"internalType":"uint256","name":"amountETHMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"addLiquidityETH","outputs":[{"internalType":"uint256","name":"amountToken","type":"uint256"},{"internalType":"uint256","name":"amountETH","type":"uint256"},{"internalType":"uint256","name":"liquidity","type":"uint256"}],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"factory","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"},{"internalType":"uint256","name":"reserveIn","type":"uint256"},{"internalType":"uint256","name":"reserveOut","type":"uint256"}],"name":"getAmountIn","outputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"reserveIn","type":"uint256"},{"internalType":"uint256","name":"reserveOut","type":"uint256"}],"name":"getAmountOut","outputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"}],"name":"getAmountsIn","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"}],"name":"getAmountsOut","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountA","type":"uint256"},{"internalType":"uint256","name":"reserveA","type":"uint256"},{"internalType":"uint256","name":"reserveB","type":"uint256"}],"name":"quote","outputs":[{"internalType":"uint256","name":"amountB","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"amountAMin","type":"uint256"},{"internalType":"uint256","name":"amountBMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"removeLiquidity","outputs":[{"internalType":"uint256","name":"amountA","type":"uint256"},{"internalType":"uint256","name":"amountB","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"amountTokenMin","type":"uint256"},{"internalType":"uint256","name":"amountETHMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"removeLiquidityETH","outputs":[{"internalType":"uint256","name":"amountToken","type":"uint256"},{"internalType":"uint256","name":"amountETH","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"amountTokenMin","type":"uint256"},{"internalType":"uint256","name":"amountETHMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"removeLiquidityETHSupportingFeeOnTransferTokens","outputs":[{"internalType":"uint256","name":"amountETH","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"amountTokenMin","type":"uint256"},{"internalType":"uint256","name":"amountETHMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"bool","name":"approveMax","type":"bool"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"removeLiquidityETHWithPermit","outputs":[{"internalType":"uint256","name":"amountToken","type":"uint256"},{"internalType":"uint256","name":"amountETH","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"amountTokenMin","type":"uint256"},{"internalType":"uint256","name":"amountETHMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"bool","name":"approveMax","type":"bool"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"removeLiquidityETHWithPermitSupportingFeeOnTransferTokens","outputs":[{"internalType":"uint256","name":"amountETH","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"amountAMin","type":"uint256"},{"internalType":"uint256","name":"amountBMin","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"bool","name":"approveMax","type":"bool"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"removeLiquidityWithPermit","outputs":[{"internalType":"uint256","name":"amountA","type":"uint256"},{"internalType":"uint256","name":"amountB","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapETHForExactTokens","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapExactETHForTokens","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapExactETHForTokensSupportingFeeOnTransferTokens","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapExactTokensForETH","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapExactTokensForETHSupportingFeeOnTransferTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapExactTokensForTokens","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapExactTokensForTokensSupportingFeeOnTransferTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"},{"internalType":"uint256","name":"amountInMax","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapTokensForExactETH","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOut","type":"uint256"},{"internalType":"uint256","name":"amountInMax","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapTokensForExactTokens","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}]'
+
+#next is the sender address, the address that hold the bnb token
+sender_address = web3.toChecksumAddress("0x05B4CCe36a206C8419B84C27Fd22bfAF64Adc268")
+
+#let's see my wallet balance
+balance = web3.eth.get_balance(sender_address)
+print(web3.fromWei(balance,'ether'))
+
+#create a contract instance using the pancakerouter contract address and its abi
+contract = web3.eth.contract(address=pancakeRouterContractAddress, abi=abi)
+
+#Contract Address of Token we want to buy
+tokenToBuy = web3.toChecksumAddress(input("Enter TokenAddress: ")) #we will but this token: 0x6615a63c260be84974166a5eddff223ce292cf3d yorkie token
+
+#we have ro specify the WBNB contract address 
+spend = web3.toChecksumAddress("0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c")  #wbnb contract
+
+#sepcify the nonce, number of transactions already done by that address.
+nonce = web3.eth.get_transaction_count(sender_address)
+
+#to time the script, to know how many mins was used
+startTime = time.time() #the start time when we began to run the script
+
+#specify the parameters for the transaction, not that the parameters must tally with the contract function
+#i.e the swapExactETHForTokens() function must be part of the funtions in the pancakeswap router CA, we can confirm this in the bscan.
+pancakeSwaptxn = contract.functions.swapExactETHForTokens(
+    #first parameter(the amount we want to swap) will be added manually so,lets jump to the next one
+    0, #set the second variable to 0, because it will be automatically calculated when we add the amount we want to purcahse
+    [spend,tokenToBuy], #add the wbnb CA and the CA of the token we want to buy
+    sender_address, #specify the receiver, we spend bnb but we want to receive the token to our address
+    (int(time.time()) + 10000) #specify deadline, incase the transaction gets stuck, we added 10k miliseconds    
+).buildTransaction({
+'from': sender_address,
+'value': web3.toWei(0.001,'ether'), #This is the Token(BNB) amount you want to Swap from
+'gas': 250000,
+'gasPrice': web3.toWei('5','gwei'),
+'nonce': nonce,
+})
+
+#Lets sign the transaction, without the need of signing manually using metamask
+#we referenced the config file and called the private key, we imported config above.
+signed_txn = web3.eth.account.sign_transaction(pancakeSwaptxn, private_key=config.private)
+
+#we need to send the transaction
+tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+
+#finally, lets print the transaction hash
+print(web3.toHex(tx_token))
+
+#run the code
+#check bscan with the hash, you will see the details and in the see more, you will see input data and the fucntion called,
+#decode then see the amount we specified and the path(the WBNB address and the CS of token bought)
+#try it again with a different token address(you can grab a token CA from CoinMarketcap or CoinGecko)
+#you can confirm all the transaxction if it went through by using the bscan to check the hash.
+ 
+
+
+link1 = 'https://www.youtube.com/watch?v=4fRAuWHPCPE'
+link2 = 'https://github.com/CodeWithJoe2020/pancakeswapBot/blob/main/cakebot.py'
+
+
+
+
+
+
+
+
+link1 = "Tutorial(How to sell token on pancakeswap in python): https://www.youtube.com/watch?v=ehWDvw0jMzo"
+linke2 = "Blockchain & A.I.- send Ethereum with just a smile: https://www.youtube.com/watch?v=krZthWO7_3Q&list=PLAgQsXt06PBghC4Sn_cWDrYiAvjn5tCSX&index=12"
+linke3 = "CodeWithJoe Playlist: https://www.youtube.com/watch?v=bqrdDrZXNTs&list=PLAgQsXt06PBghC4Sn_cWDrYiAvjn5tCSX"
+linke4 = "CodewithJoe Videos: https://www.youtube.com/@CodeWithJoe/videos"
+linke5 = "TokenLaunch SniperBo: https://www.youtube.com/watch?v=jY3iVnQPqsg"
